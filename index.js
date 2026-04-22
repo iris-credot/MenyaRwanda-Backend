9ec17893218cc71e79ed5fb9c1bc9309a5d6967d
iris-credot <tetairiscredot@gmail.com>
2026-04-23 01:12:46 +0200

    Installed all dependencies

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+ const cors = require('cors');
+// const swaggerUi = require('swagger-ui-express');
+//const swagger = require('./swagger.json');
+const dotenv= require('dotenv');
+dotenv.config();
+const connection = process.env.MONGODB_URI ;
+const port = process.env.PORT ;
+const app = express();
+//const errorHandling = require('./Middleware/errorHandler');
+//const AllRoutes = require('./Routes/app');
+/**
+ app.use(cors({
+    origin: ["http://localhost:3000","http://localhost:5174","https://bistro-pulse-front-end-k4d8.vercel.app"],
+     credentials: true,
+     allowedHeaders: ['Content-Type', 'Authorization'],
+     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
+ }));
+ */
+ 
+app.use(express.json());
+app.use(cookieParser());
+//app.use('/bistrouSwagger', swaggerUi.serve, swaggerUi.setup(swagger))
+
+mongoose.connect(connection)
+.then(() => {
+    app.listen(port, () =>{
+        console.log("Mongo DB connected....")
+        console.log(`Server running on ${port}...`);
+    })
+})
+.catch((err) => console.log(err));
+
+ //app.use('/api', AllRoutes);
+//app.use(errorHandling);
