d4eb3a6c33c259c7a1d99d58b26b215634e48752
iris-credot <tetairiscredot@gmail.com>
2026-04-25 23:39:48 +0200

    addded the swagger authirization

--- Middleware/async.js
+++ Middleware/async.js
@@ -1,13 +1,7 @@
 const asyncWrapper = (fn) => {
-    return async (req, res, next) => {
-      try {
-        await fn(req, res, next)
-      } catch (err) {
-        next(err)
-        // console.log(err);
-      }
-    }
-  }
-  
-  module.exports = asyncWrapper
-  
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
+
+module.exports = asyncWrapper;
