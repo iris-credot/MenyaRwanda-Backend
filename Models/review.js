a49f5d5bff3ea48a788ed7ffe881a7bb4e88e391
iris-credot <tetairiscredot@gmail.com>
2026-04-26 02:16:23 +0200

    addded the swagger authirization

---
+++ Models/review.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+
+  attractionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Attraction',
+    required: true
+  },
+
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+
+  comment: {
+    type: String
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Review', reviewSchema);
