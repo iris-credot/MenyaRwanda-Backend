9e26e55332cfebe04ac612d44cbd657f40a499cc
Teta Iris Credot <tetairiscredot@gmail.com>
2026-05-10 21:05:43 +0200

    updated the swagger

--- config/gemini.js
+++ config/gemini.js
@@ -3,7 +3,7 @@ if (!process.env.GOOGLE_API_KEY) {
   throw new Error("GOOGLE_API_KEY is missing from environment variables");
 }
 const model = new ChatGoogleGenerativeAI({
-  model: "gemini-2.0-flash",
+  model: "gemini-1.5-flash-8b",
   apiKey: process.env.GOOGLE_API_KEY,
   temperature: 0.3,
   streaming: true,  
