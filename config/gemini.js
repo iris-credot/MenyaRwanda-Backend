7f8fb37d7e9718a7280f6304d31bdaaf0809516e
Teta Iris Credot <tetairiscredot@gmail.com>
2026-05-10 20:33:57 +0200

    trained the model on our databse

--- config/gemini.js
+++ config/gemini.js
@@ -3,7 +3,7 @@ if (!process.env.GOOGLE_API_KEY) {
   throw new Error("GOOGLE_API_KEY is missing from environment variables");
 }
 const model = new ChatGoogleGenerativeAI({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.0-flash",
   apiKey: process.env.GOOGLE_API_KEY,
   temperature: 0.3,
 });
