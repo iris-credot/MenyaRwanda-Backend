27dfb4d5963481a11e4fbfad23526566084830b4
Iris Credot TETA <137190728+iris-credot@users.noreply.github.com>
2026-05-10 21:20:50 +0200

    addd another ai

--- config/gemini.js
+++ config/gemini.js
@@ -1,13 +1,13 @@
-const { ChatGoogleGenerativeAI } = require("@langchain/google-genai");
-if (!process.env.GOOGLE_API_KEY) {
-  throw new Error("GOOGLE_API_KEY is missing from environment variables");
+const { ChatGroq } = require("@langchain/groq");
+
+if (!process.env.GROQ_API_KEY) {
+  throw new Error("GROQ_API_KEY is missing from environment variables");
 }
-const model = new ChatGoogleGenerativeAI({
-  model: "gemini-1.5-flash-8b",
-  apiKey: process.env.GOOGLE_API_KEY,
+
+const model = new ChatGroq({
+  model: "llama-3.1-8b-instant",
+  apiKey: process.env.GROQ_API_KEY,
   temperature: 0.3,
-  streaming: true,  
 });
 
 module.exports = model;
-
